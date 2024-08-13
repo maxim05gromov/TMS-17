a9c4a2496a89dd8de4d1f6566930c170212c58ae
Maksim <i@mgromov.ru>
2024-08-13 14:40:43 +0300

    Initial Commit

---
+++ HW17/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW17
+//
+//  Created by Максим Громов on 13.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
